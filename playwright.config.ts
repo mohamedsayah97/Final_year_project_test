37873844f222702f34946a5d2110192b934fbdad
mohamedsayah97 <mohamedsayah633@gmail.com>
2026-06-07 23:05:05 +0100

    fixing ci error

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,12 +69,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:5173',
-    reuseExistingServer: false,
-    timeout: 120000,
-  },
 });
